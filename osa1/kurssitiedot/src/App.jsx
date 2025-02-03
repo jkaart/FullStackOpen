e1667fd15796d944fda9d2389b44c498c4a911dd
Jani Kaartinen <jkaart@gmail.com>
2025-02-03 15:20:24 +0200

    exercise 1.1-1.2

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = (props) => (
+  <>
+    <h1>{props.course}</h1>
+  </>
+)
+
+const Part = (props) => (
+  <>
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  </>
+)
+
+const Content = (props) => (
+  <div>
+    <Part part={props.part[0]} exercises={props.exercises[0]} />
+    <Part part={props.part[1]} exercises={props.exercises[1]} />
+    <Part part={props.part[2]} exercises={props.exercises[2]} />
+  </div>
+)
+
+const Total = (props) => {
+  const total = props.exercises[0] + props.exercises[1] + props.exercises[2]
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  )
+}
+
+export default App
