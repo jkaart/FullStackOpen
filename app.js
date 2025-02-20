8087bc07ccb772345fe577009cbb42b585363b36
Jani Kaartinen <jkaart@gmail.com>
2025-02-20 13:38:21 +0200

    routers renamed to controllers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./routers/blogs')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
