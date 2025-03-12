d1649cb78a3fd1f576af0b0944ad27568d16879d
Jani Kaartinen <jkaart@gmail.com>
2025-03-12 14:29:10 +0200

    exercise 6.9

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -2,7 +2,13 @@ import { useSelector } from 'react-redux'
 import Anecdote from './Anecdote'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    if (state.filter === '') {
+      return state.anecdotes
+    }
+    return state.anecdotes.filter(anecdote => anecdote.content.includes(state.filter))
+    
+  })
   const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
 
   return (
